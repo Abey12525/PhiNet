1855cb2764139ae2afc3aca38f6579d13a667bfe
Abey12525 <abeyrhurtis@cs.ajce.in>
2019-03-31 19:10:54 +0530

    initial_commit 1.0.3

--- TestingFile.py
+++ TestingFile.py
@@ -33,25 +33,34 @@ neuron_num = np.array(neuron_num)
 #tf.set_random_seed(10)
 shp = neuron_num.shape[0]
 print(shp)
+# weight = {
+#     w0 : tf.get_variables("w0",shape = (1,10,1),initializer = tf.contrib.layers.xaviour_initializer())
+# }
 x = tf.placeholder(dtype = tf.float32,shape=[None,1,shp])
 # Culstm = tf.contrib.cudnn_rnn.CudnnLSTM(10,10)
 # output, state = Culstm(x,initial_state = None, training = True)
-# Culstm = tf.contrib.cudnn_rnn.CudnnLSTM(20,30)
-#output1, state1 = Culstm(x,initial_state = None,training = True)
-Culstm = tf.contrib.cudnn_rnn.CudnnLSTM(4,1)
-output2,state2 = Culstm(x,initial_state = None,training = True)
-Culstm2 = tf.contrib.cudnn_rnn.CudnnLSTM(4,15)
-output3,state2 = Culstm2(output2,initial_state = state2, training = True)
+Culstm = tf.contrib.cudnn_rnn.CudnnLSTM(25,10)
+output1, state1 = Culstm(x,initial_state = None,training = True)
+#dense = tf.layers.Dense(output1,activation = tf.nn.softmax)
+Culstm1 = tf.contrib.cudnn_rnn.CudnnLSTM(15,10)
+output2,state2 = Culstm1(output1,initial_state = state1,training = True)
+Culstm2 = tf.contrib.cudnn_rnn.CudnnLSTM(10,1)
+output3,state3 = Culstm2(output2,initial_state = None, training = True)
+Culstm3 = tf.contrib.cudnn_rnn.CudnnLSTM(20,3)
+output4,state4 = Culstm3(output3,initial_state = state3, training = True)
+# Culstm5 = tf.contrib.cudnn_rnn.CudnnLSTM(10,10)
+# output5,state5 = Culstm5(output4,initial_state = state4, training = True)
 init = tf.global_variables_initializer()
 # with tf.variable_scope(reuse=tf.AUTO_REUSE):
 #     x = sess.run(output, feed_dict={x : [re,re]})
 #     print("Test Phase one complete ")
 with tf.Session() as sess:
-
     sess.run(init)
     writer = tf.summary.FileWriter('./graphs1',sess.graph)
-    xi = sess.run(output2,feed_dict={x : [[neuron_num]]})
-    print(xi)
+    #a,b,c,d,e,f,g,h,i,j= sess.run([output1,state1,output2,state2,output3,state3,output4,state4,output5,state5],feed_dict={x : [[neuron_num]]})
+    output= sess.run(output4,feed_dict = { x : [[neuron_num]]})
+    print(output)
+
     print("##########################################")
 
 
--- TestingFile_sub1.py
+++ TestingFile_sub1.py
@@ -1,30 +1,40 @@
 import tensorflow as tf
-print(tf.__version__)
-import InitRand
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+tf.reset_default_graph
+saver = tf.train.Saver()
+x = tf.placeholder(dtype=tf.float32,shape=[None, 1, 1])
+c = tf.contrib.cudnn_rnn.CudnnLSTM(10,5)
+output, state = c(x,initial_state = None, training = True)
+d = tf.contrib.cudnn_rnn.CudnnLSTM(10,4)
+out,st = d(output,initial_state = None, training = True)
+e = tf.contrib.cudnn_rnn.CudnnLSTM(6,3)
+out1,st1 = e(out,initial_state = None, training = True)
+f = tf.contrib.cudnn_rnn.CudnnLSTM(10,1)
+out2,st2 = f(out1,initial_state = None,training = True)
 
-# class Ne:
-#     init_scale = 0.1
-#
-#
-# flags = tf.flags
-#
-# flags.DEFINE_string("model","small","testing")
-#
-#
-#
-# FLAGS = flags.FLAGS
-# print("model is :",FLAGS.model)
-#
-# if FLAGS.model == "small":
-#     print("test phase_one complete :",FLAGS.model)
-#     obj = Ne
-#
-#
-# def test(model):
-#     print(model.init_scale)
-#
-# test(obj)
-
-
-x = InitRand.RandomInit()
-print(x.rnn_reward_rnd_init())
+"""
+tf.contrib.layers.fully_connected(
+    inputs,
+    num_outputs,
+    activation_fn=tf.nn.relu,
+    normalizer_fn=None,
+    normalizer_params=None,
+    weights_initializer=initializers.xavier_initializer(),
+    weights_regularizer=None,
+    biases_initializer=tf.zeros_initializer(),
+    biases_regularizer=None,
+    reuse=None,
+    variables_collections=None,
+    outputs_collections=None,
+    trainable=True,
+    scope=None
+)
+"""
+with tf.Session() as sess:
+    Writer = tf.summary.FileWriter(logdir='./graphs/G{}/'.format(timestr),graph=sess.graph)
+    sess.run(tf.global_variables_initializer())
+    x = sess.run(out2,feed_dict={x : [[[20]]]})
+    save_path = saver.save(sess, "./tmp/model.ckpt")
+    print("Model saved in path: %s" % save_path)
+    print(x)
