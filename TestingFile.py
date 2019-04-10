93e282629cd17df4018c5035e1ec622077a3b472
Abey12525 <abeyrhurtis@cs.ajce.in>
2019-04-10 13:55:08 +0530

    initial_commit 1.0.8

--- TestingFile.py
+++ TestingFile.py
@@ -1,50 +1,69 @@
 import tensorflow as tf
 import numpy as np
-import SoftC as sft
-import matplotlib.pyplot as plt
-mnist = tf.keras.datasets.mnist
+
+# def data_gen():
+#     tf.reset_default_graph()
+#     k = np.random.uniform(2,15)
+#     k = int(np.round(k))
+#     var_list = []
+#     rw_list = []
+#     for i in range(k):
+#         x = np.random.uniform(0, 100)
+#         rw_list.append(x)
+#         vars()["vk{}".format(i)] = tf.get_variable("vk{}".format(i),shape = [int(np.round(np.random.uniform(200,500)))],dtype = tf.float32 ,initializer =tf.contrib.layers.xavier_initializer())
+#         tmp = eval("vk{}".format(i))
+#         var_list.append(tmp)
+#     var_list.append(x)
+#     return var_list,rw_list
+
+# def Model(data,rw):
+#     x = tf.placeholder(dtype = tf.float32, shape=[None,1,1])
+#     model = tf.contrib.cudnn_rnn.CudnnLSTM(10,5)
+#     out,state = model(x,initial_state = None, training = True)
+#     rw = tf.Variable([-5],dtype=tf.float32)
+#     opt = tf.train.GradientDescentOptimizer(out).minimize(rw)
+#     init = tf.global_variables_initializer()
+#     with tf.Session() as sess:
+#         sess.run(init)
+#         for i in range(100):
+#             x = sess.run(opt,feed_dict={x : [[[data]]]})
+#             if(i%25 == 0):
+#                 print("optimizing : ",x)
+
+
+from tensorflow.keras.datasets import mnist
 (train_x,train_y),(test_x,test_y) = mnist.load_data()
-# img = np.array(img)
-# img = img.reshape((28,28))
-# plt.imshow(img,cmap='gray')
-# plt.show()
-"""tf.reset_default_graph()"""
-#import SoftC as Sft
-# inp =tf.constant([[[3562,6,451,512],[15,24,543,457],[563,4642,464,645],[3,23,2,45],[46,575,7,54],[34,56,5,64],[3,65,43,456],[65,34,7,8]]])
-# v1 = tf.layers.dense(inp,units=10 ,activation = tf.nn.relu)
-# #v2 = tf.layers.dense(v1,units=1024 ,activation = tf.nn.relu )
-# init = tf.global_variables_initializer()
-# with tf.Session() as sess:
-#     sess.run(init)
-#     print(sess.run(v1))
-# var,shp = Sft.SoftCinit()
-"""Place holder test"""
-sftc = sft.SoftCinit()
-inp = sftc.rnd_init()
-#inp = tf.Variable([[3,4,5,6]])
-# t = tf.add(inp,4)
-#inp = tf.get_variable("v1", shape=[1], initializer=tf.contrib.layers.xavier_initializer())
-#img = np.reshape(train_x,(60000,784))
-shp = np.random.randint(500,784)
-img = tf.get_variable("bkn",shape=shp,initializer=tf.contrib.layers.xavier_initializer())
-print(img,shp)
-for x in inp:
-    var = x[0]
-    shpa = x[1]
-    break
-print(var,shpa)
-x = tf.placeholder(tf.float32, [None,shpa])
-model = tf.layers.dense(x, units=1000,activation=tf.nn.relu)
-model = tf.layers.dense(model,units = 500,activation=tf.nn.relu)
-model = tf.layers.dense(model,units = 10)
-# cost = tf.reduce_mean((model-x)**2)
-# opt = tf.train.RMSPropOptimizer(0.01).minimize(cost)
-init = tf.global_variables_initializer()
-# tf.summary.scalar("cost",cost)
-# merge_summary_op  = tf.summary.merge_all()
+
+x = tf.shape(train_x)
 with tf.Session() as sess:
-    sess.run(init)
-    img = [np.array(sess.run(var))]
-    print(img)
-    result_weight = sess.run(model,feed_dict={x : img})
-    print(result_weight)
+    x = sess.run(x)
+    y = []
+    z = x[1:].astype(int)
+    z = z[:]
+    y.append(None)
+    for j in z:
+        print(j)
+        y.append(j)
+
+# rw = [[[0],[4],[5],[-5]]]
+# Model([[[3,5,2,7],[4,5,6,8],[1,9,2,5],[4,6,8,7]]],rw)
+
+
+#
+# import time
+# import sys
+#
+# toolbar_width = 40
+#
+# # setup toolbar
+# sys.stdout.write("[%s]" % (" " * toolbar_width))
+# sys.stdout.flush()
+# sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+#
+# for i in range(toolbar_width):
+#     time.sleep(0.1) # do real work here
+#     # update the bar
+#     sys.stdout.write("-")
+#     sys.stdout.flush()
+#
+# sys.stdout.write("\n")
